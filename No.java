1f75ac7499cbafe436fa93beb61d24d470c51e61
babi <141651503+babitasz@users.noreply.github.com>
2024-08-15 18:48:21 -0300

    Update comentários No.java

--- No.java
+++ No.java
@@ -6,6 +6,7 @@ public class No {
     // e criar uma ligação entre os elementos.
     // Cada objeto 'No' contém um valor e uma referência para o
     // próximo nó na estrutura (proximo).
-    //cada caixinha da pilha tem um dado e a informação de quem é o elemento que está embaixo dela.
-    //a pilha acaba quando o ponteiro aponta para NULL, quando não há mais elementos embaixo.
+    // cada caixinha da pilha tem um dado e a informação de quem é o elemento que está embaixo dela.
+    // a pilha acaba quando o ponteiro aponta para NULL, quando não há mais elementos embaixo.
+    // o ponteiro fica emcima dos elementos, já que precisa "apontar" pra eles, logo, se remover o ultimo elemento ele apontará para o null.
 }
