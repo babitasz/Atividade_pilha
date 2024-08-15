a3ef623a9c55f7d00de1e1f18d06410692781db0
babitasz <barbaratippa@gmail.com>
2024-08-15 18:46:22 -0300

    pilha estática e dinâmica com comentários!! wow 😘❤️🧸✨

--- No.java
+++ No.java
@@ -1,4 +1,11 @@
 public class No {
-    int data;
-    No proximo; //ponteiro para o proximo elemento da pilha | mostra quem é o proximo elemento
+    int data; // valor armazenado neste nó da pilha
+    No proximo; // referência (ponteiro) para o próximo nó na pilha | indica qual é o próximo elemento na pilha
+
+    // A classe é usada para armazenar valores
+    // e criar uma ligação entre os elementos.
+    // Cada objeto 'No' contém um valor e uma referência para o
+    // próximo nó na estrutura (proximo).
+    //cada caixinha da pilha tem um dado e a informação de quem é o elemento que está embaixo dela.
+    //a pilha acaba quando o ponteiro aponta para NULL, quando não há mais elementos embaixo.
 }
